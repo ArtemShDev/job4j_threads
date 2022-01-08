9924b0e80f654ed33a968d0e89a813e5c56fd3cd
ArtemShDev <dev.art.balt@gmail.com>
2022-01-08 15:08:32 +0300

    4. Скачивание файла с ограничением. [#144271]

---
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -0,0 +1,45 @@
+package ru.job4j.thread;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+            FileOutputStream fileOutputStream = new FileOutputStream("temp1.tmp")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                if (bytesRead == 1024) {
+                    long delta = System.currentTimeMillis() - start;
+                    if (delta < speed) {
+                        Thread.sleep(speed - delta);
+                    }
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
